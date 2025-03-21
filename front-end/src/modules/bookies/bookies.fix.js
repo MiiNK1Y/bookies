34ea0f0a5498d572fecfe1f072be6702bdd26e5d
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2025-03-21 11:00:07 +0100

    cleaned up, FlatParent now extends Rebuild and uptimized code reuse

--- front-end/src/modules/bookies/bookies.fix.js
+++ front-end/src/modules/bookies/bookies.fix.js
@@ -4,111 +4,79 @@
 
 
 export class Rebuild {
-  #flatArray;
-  #tempArray;
-  #inflated;
+  constructor(flatBookies, tempArray = []) {
+    this.flatArray = flatBookies;
+    this.tempArray = tempArray;
+    this.inflated = [];
 
-  constructor(flatBookies) {
-    this.#flatArray = flatBookies;
-    this.#tempArray = [];
-    this.#inflated = [];
-
-    this.#rebuildFromFlat();
+    if (new.target === Rebuild) this.rebuild();
   }
 
-  #removeItemFromFlatArray(item) {
-    this.#flatArray = this.#flatArray.filter((a) => a.Id != item.Id);
-  }
-
-  #removeItemFromTempArray(item) {
-    this.#tempArray = this.#tempArray.filter((a) => a.Id != item.Id);
-  }
-
-  #parentStillInFlatArray = (item) => {
-    const parents = this.#flatArray.filter(a => a.Type == "Folder");
+  parentStillInFlatArray = (item) => {
+    const parents = this.flatArray.filter(a => a.Type == "Folder");
     const parent = parents.find(a => a.Children.includes(item.Id));
     if (parent) return true;
     else return false;
   }
 
-  #rebuildFromFlat = () => {
-    this.#flatArray.forEach(item => {
-      if (!this.#flatArray.includes(item)) return;
+  removeItemFromCollection(item) {
+    this.flatArray = this.flatArray.filter((a) => a.Id != item.Id);
+    this.tempArray = this.tempArray.filter((a) => a.Id != item.Id);
+  }
 
-      if (this.#parentStillInFlatArray(item)) {
-        this.#tempArray.push(item);
+  rebuild() {
+    this.flatArray.forEach(item => {
+      if (!this.flatArray.includes(item)) return;
+
+      if (this.parentStillInFlatArray(item)) {
+        this.tempArray.push(item);
         return;
       }
 
-      this.#removeItemFromFlatArray(item);
-      this.#removeItemFromTempArray(item);
+      this.removeItemFromCollection(item);
 
       let parent;
       switch (item.Type) {
         case "Folder":
-          parent = new FlatParent(item, this.#flatArray, this.#tempArray);
-          this.#flatArray = parent.flatArray;
-          this.#tempArray = parent.tempArray;
-          this.#inflated.push(parent.parent);
+          parent = new FlatParent(item, this.flatArray, this.tempArray);
+          this.flatArray = parent.flatArray;
+          this.tempArray = parent.tempArray;
+          this.inflated.push(parent.parent);
           break;
 
         case "Bookmark":
-          this.#inflated.push(item);
+          this.inflated.push(item);
           break;
       }
     })
   };
-
-  get inflated() {
-    return this.#inflated;
-  }
 }
 
 
-export class FlatParent {
-  #parent;
-  #flatArray;
-  #tempArray;
-
+export class FlatParent extends Rebuild {
   constructor(parent, flatArray, tempArray) {
-    this.#parent = parent;
-    this.#flatArray = flatArray;
-    this.#tempArray = tempArray;
+    super(flatArray, tempArray);
+    this.parent = parent;
 
-    this.#prepareForWalk();
-    if (this.#parent.Children.length > 0) {
-      this.#walk();
-      this.#checkComplete();
+    this.prepareForWalk();
+    if (this.parent.Children.length > 0) {
+      this.walk();
+      this.checkComplete();
     }
-    this.#prepareInflated();
+    this.prepareInflated();
   }
 
-  #checkComplete(){
-    const childrenIDs = this.#parent.Bookmarks.map(a => a.Id);
-    const isComplete = this.#parent.Children.every(a => childrenIDs.includes(a));
-    if (isComplete) return true;
-    throw new Error(`Error: ${this.#parent.Id} is NOT complete!`);
-  };
-
-  #prepareForWalk() {
-    this.#parent.Bookmarks = [];
+  prepareForWalk() {
+    this.parent.Bookmarks = [];
   }
 
-  #prepareInflated() {
-    delete this.#parent.Children;
+  prepareInflated() {
+    delete this.parent.Children;
   }
 
-  #removeItemFromFlatArray(item) {
-    this.#flatArray = this.#flatArray.filter((a) => a.Id != item.Id);
-  }
-
-  #removeItemFromTempArray(item) {
-    this.#tempArray = this.#tempArray.filter((a) => a.Id != item.Id);
-  }
-
-  #flatChildItem = (childId) => {
-    const flatItem = this.#flatArray.find(b => b.Id == childId);
-    const tempItem = this.#tempArray.find(b => b.Id == childId);
+  flatChildItem = (childId) => {
+    const flatItem = this.flatArray.find(b => b.Id == childId);
+    const tempItem = this.tempArray.find(b => b.Id == childId);
     const item = flatItem ?? tempItem;
 
     if (!item) throw new Error(`Error: Flat child with ID [${childId}] ` +
@@ -117,37 +85,32 @@ export class FlatParent {
     return item;
   };
 
-  #walk() {
-    this.#parent.Children.forEach(a => {
-      const item = this.#flatChildItem(a);
-      this.#removeItemFromFlatArray(item);
-      this.#removeItemFromTempArray(item);
+  walk() {
+    this.parent.Children.forEach(a => {
+      const item = this.flatChildItem(a);
+
+      this.removeItemFromCollection(item);
 
       let parent;
       switch (item.Type) {
         case "Folder":
-          parent = new FlatParent(item, this.#flatArray, this.#tempArray);
-          this.#flatArray = parent.flatArray;
-          this.#tempArray = parent.tempArray;
-          this.#parent.Bookmarks.push(parent.parent);
+          parent = new FlatParent(item, this.flatArray, this.tempArray);
+          this.flatArray = parent.flatArray;
+          this.tempArray = parent.tempArray;
+          this.parent.Bookmarks.push(parent.parent);
           break;
 
         case "Bookmark":
-          this.#parent.Bookmarks.push(item);
+          this.parent.Bookmarks.push(item);
           break;
       }
     });
+  }
+
+  checkComplete() {
+    const childrenIDs = this.parent.Bookmarks.map(a => a.Id);
+    const isComplete = this.parent.Children.every(a => childrenIDs.includes(a));
+    if (isComplete) return true;
+    throw new Error(`Error: ${this.parent.Id} is NOT complete!`);
   };
-
-  get parent() {
-    return this.#parent;
-  }
-
-  get flatArray() {
-    return this.#flatArray;
-  }
-
-  get tempArray() {
-    return this.#tempArray;
-  }
 }
