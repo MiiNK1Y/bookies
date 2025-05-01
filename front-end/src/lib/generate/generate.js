62b6f2a92311b7d3eb8b4d47c975847bcb12c14b
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2025-05-01 20:38:59 +0200

    ...

---
+++ front-end/src/lib/generate/generate.js
@@ -0,0 +1,89 @@
+export class Generate() {
+  constructor(template, min, max) {
+    this.template = template;
+    this.min = min;
+    this.max = max;
+
+    this.tempConstructedData;
+    this.return = [];
+
+    this.commandKeys = [
+      "$TYPE",
+      "$MIN",
+      "$MAX",
+      "$COLLECTION"
+    ];
+
+    this.commandValues = [
+      "$STR_LOREM_IPSUM",
+      "$STR_RANDOM",
+      "$INT_INCREMENT_GLOBAL",
+      "$ARR_RANDOM",
+      "$ARR_NEST_WITH_SELF"
+    ];
+
+    this.#parseTemplate();
+  }
+
+
+  loremIpsumSnippet(charAmount) {
+    const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing " +
+      "elit. In interdum ullamcorper odio nec egestas. Aliquam consequat " +
+      "ipsum id nibh rutrum elementum. Vivamus massa odio, consequat vitae " +
+      "nunc non, posuere convallis massa. Aliquam sed arcu ac arcu ultrices " +
+      "hendrerit viverra ac lorem. Phasellus vel hendrerit augue, at mollis " +
+      "justo. Proin elementum, ante nec tempor lobortis, mauris sapien " +
+      "posuere turpis, quis molestie nisl augue in est. Nullam ullamcorper " +
+      "diam a diam lobortis blandit. Vivamus pretium egestas egestas. Etiam " +
+      "in mi vel odio porta luctus at vitae sem. Nulla ut tellus bibendum, " +
+      "suscipit leo et, placerat lectus. Vestibulum non nulla erat.";
+
+    const loremIpsumLength = loremIpsum.length;
+
+    if (charAmount > loremIpsumLength) {
+      throw new Error(`The Lorem Ipsum snippet is only ${loremIpsumLength}` +
+        `characters long, ${charAmount} was requested.`);
+    }
+
+    return loremIpsum.substring(charAmount);
+  }
+
+
+  #parseCommandKey(key) {
+    switch (key) {
+      case "$TYPE":
+        break;
+      case "$MIN":
+        break;
+      case "$MAX":
+        break;
+      case "$COLLECTION":
+        break;
+
+      default:
+        break;
+    }
+  }
+
+
+  #parseCommandValue() {
+    //
+  }
+
+
+  #parseTemplate() {
+    for (const item of this.template) {
+      for (const [key, value] in Object.entries(this.template)) {
+
+        const keyIsCommand = this.magicKeys.contains(key);
+        const valueIsCommand = this.magicValues.contains(value);
+
+        if (keyIsCommand) {
+          this.#parseCommandKey();
+        } else if (valueIsCommand) {
+          //
+        }
+      }
+    }
+  }
+}
---
+++ front-end/src/lib/generate/validateTemplate.js
@@ -0,0 +1,110 @@
+export class ValidateGeneratorTemplate() {
+  constructor(template) {
+    this.template = template;
+
+    this.commandKeys = [
+      "$TYPE",
+      "$MIN",
+      "$MAX",
+      "$COLLECTION"
+    ];
+
+    this.commandValues = [
+      "$STR_LOREM_IPSUM",
+      "$STR_RANDOM",
+      "$INT_INCREMENT_GLOBAL",
+      "$ARR_RANDOM",
+      "$ARR_NEST_WITH_SELF"
+    ];
+
+    this.checkTemplateType();
+    this.checkTemplateItemsTypes();
+    this.checkTemplateCommands();
+  }
+
+
+  typeKeyValueIsValid = (value) => {
+    value === "$STR_LOREM_IPSUM" ||
+    value === "$STR_RANDOM" ||
+    value === "$ARR_RANDOM" ||
+    value === "$ARR_NEST_WITH_SELF"
+  };
+
+
+  // $MIN only allows for positive integer numbers.
+  minKeyValueIsValid = (value) => {
+    (typeof value) === "Number" &&
+    !(value.includes("-"))
+  };
+
+
+  // $MAX only allows for positive integer numbers.
+  maxKeyValueIsValid = (value) => {
+    (typeof value) === "Number" &&
+    !(value.includes("-"))
+  };
+
+
+  /*
+  * $COLLECTION takes an array of different values to be picked from \
+  * and thus allow for pretty much anything.
+  */
+  collectionKeyValueIsValid = (value) => {
+    (typeof value) === "Array" &&
+    value.length >= 1
+  };
+
+
+  checkCommandKeyValuePairs(key, value) {
+    const combinationError = new Error("ERROR: the command combination: " +
+      `${key, value} is not a valid combination.`);
+
+    switch (key) {
+      case "$TYPE":
+        if (!this.typeKeyValueIsValid(value)) Throw combinationError;
+        break;
+      case "$MIN":
+        if (!this.minKeyValueIsValid(value)) Throw combinationError;
+        break;
+      case "$MAX":
+        if (!this.maxKeyValueIsValid(value)) Throw combinationError;
+        break;
+      case "$COLLECTION":
+        if (!this.collectionKeyValueIsValid(value)) Throw combinationError;
+        break;
+      default:
+        return;
+    }
+  }
+
+
+  // Check that the given template is an array.
+  checkTemplateType() {
+    if ((typeof this.template) !== "Array") {
+      Throw new Error("ERROR: The given template need to be in an array.");
+    }
+  }
+
+
+  // Check that all items in the given template array is an object.
+  checkTemplateItemsTypes() {
+    for (const item of this.template) {
+      if ((typeof item) !== "Object") {
+        Throw new Error("ERROR: All items in a template array need to be " +
+          "defined by an object.");
+      }
+    }
+  }
+
+
+  checkTemplateCommands() {
+    for (const item of this.template) {
+      const itemKeys = Object.keys(item);
+      const itemValues = Object.values(item);
+
+      for (const [key, value] in Object.entries(item)) {
+        this.checkCommandKeyValuePairs(key, value);
+      }
+    }
+  }
+}
